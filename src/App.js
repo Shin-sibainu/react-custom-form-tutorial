6e294ac5da30ee9877cfb136f5ec4c5fb3a3f048
Shin-sibainu <shincode0712@gmail.com>
2022-03-27 14:19:29 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const initialValues = { username: "a", mailAddress: "", password: "" };
+  const initialValues = { username: "", mailAddress: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
   const [formErros, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
     // console.log(e.target.name);
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
-    console.log(formValues);
+    // console.log(formValues);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormErrors(validate(formValues));
+    setIsSubmit(true);
   };
 
+  useEffect(() => {
+    console.log(formErros);
+    //エラーなしでかつ送信されているなら。
+    if (Object.keys(formErros).length === 0 && isSubmit) {
+      console.log(formValues);
+    } else {
+    }
+  }, [formErros]);
+
   //バリデーションチェック
-  const validate = (values) => {};
+  const validate = (values) => {
+    const errors = {};
+    //半角英数字のみ(空文字OK)
+    const regex =
+      /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+    //valueが空ならerrrosの配列に入れる。
+    if (!values.username) {
+      errors.username = "ユーザー名を入力してください。";
+    }
+    if (!values.mailAddress) {
+      errors.mailAddress = "メールアドレスを入力してください。";
+    } else if (!regex.test(values.mailAddress)) {
+      errors.mailAddress = "正しいメールアドレスを入力してください";
+    }
+    if (!values.password) {
+      errors.password = "パスワードを入力してください。";
+    } else if (values.password.length < 4) {
+      errors.password = "4文字以上15文字以下のパスワードを入力してください";
+    } else if (values.password.length > 15) {
+      errors.password = "4文字以上15文字以下のパスワードを入力してください";
+    }
+    return errors;
+  };
 
   return (
     <div className="formContainer">
@@ -37,26 +70,34 @@ function App() {
               onChange={(e) => handleChange(e)}
             />
           </div>
+          <p className="errorMsg">{formErros.username}</p>
           <div className="formField">
             <label>メールアドレス</label>
             <input
               type="text"
-              name="mailaddress"
+              name="mailAddress"
               placeholder="メールアドレス"
               value={formValues.mailAddress}
+              onChange={(e) => handleChange(e)}
             />
           </div>
+          <p className="errorMsg">{formErros.mailAddress}</p>
+
           <div className="formField">
             <label>パスワード</label>
-
             <input
               type="text"
               name="password"
               placeholder="パスワード"
               value={formValues.password}
+              onChange={(e) => handleChange(e)}
             />
           </div>
+          <p className="errorMsg">{formErros.password}</p>
           <button className="submitButton">登録</button>
+          {Object.keys(formErros).length === 0 && isSubmit && (
+            <div className="msgOk">ログインに成功しました</div>
+          )}
         </div>
       </form>
     </div>
